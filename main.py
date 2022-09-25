e954f42a02f83a4938e3e5ed2530eed112923f86
Nurbaya25 <syahrunfacrezy@gmail.com>
2022-09-26 04:24:09 +0900

    DDOS RF

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from select import select
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 
